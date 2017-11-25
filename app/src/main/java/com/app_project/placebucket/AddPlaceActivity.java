be75f9a4e3062b310a24a7570c78cad1c0b29e85
parkeunsoo <1426312@naver.com>
2017-11-25 19:06:44 +0900

    1115_EUNSOO

--- app/src/main/java/com/app_project/placebucket/AddPlaceActivity.java
+++ app/src/main/java/com/app_project/placebucket/AddPlaceActivity.java
@@ -12,6 +12,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -22,16 +23,20 @@ import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.location.places.GeoDataApi;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.PlaceFilter;
 import com.google.android.gms.location.places.PlacePhotoMetadata;
 import com.google.android.gms.location.places.PlacePhotoMetadataBuffer;
 import com.google.android.gms.location.places.PlacePhotoMetadataResult;
+import com.google.android.gms.location.places.PlacePhotoResult;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.maps.GoogleMap;
+
 import android.support.v4.app.FragmentActivity;
 
 import org.json.JSONException;
@@ -44,13 +49,15 @@ import java.net.URL;
 
 
 public class AddPlaceActivity extends AppCompatActivity implements OnConnectionFailedListener{
-
+    ImageView mImageView;
     ImageView backButton;
+    TextView mText1;
+    TextView mText2;
     public GoogleApiClient mGoogleApiClient;
+    public GoogleMap mMap;
     private ProgressDialog pDialog;
     private static final String url_check_place = "http://18.216.36.241/pb/check_place.php";
     private static final String url_add_place = "http://18.216.36.241/pb/add_place.php";
-
     private static final int REQUEST_PLACE_PICKER = 1;
     private static  Place place ;
     private static  String id ;
@@ -74,8 +81,9 @@ public class AddPlaceActivity extends AppCompatActivity implements OnConnectionF
                 .addApi(Places.PLACE_DETECTION_API)
                 .enableAutoManage(this, this)
                 .build();
-
-
+        mText1 = findViewById(R.id.textview1);
+        mText2 = findViewById(R.id.textview2);
+        mImageView = findViewById(R.id.imageView2);
         backButton = findViewById(R.id.back_button_2);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -266,6 +274,7 @@ public class AddPlaceActivity extends AppCompatActivity implements OnConnectionF
 
                         if (success == 1) {
                             Toast.makeText(getApplicationContext(), jsonObject.getString("message"), Toast.LENGTH_LONG).show();
+                            new PhotoTask(26,26).execute(id);
                             setResult(Activity.RESULT_OK);
                             finish();
 
@@ -287,6 +296,92 @@ public class AddPlaceActivity extends AppCompatActivity implements OnConnectionF
                 Toast.makeText(getApplicationContext(), "JSON response is null.", Toast.LENGTH_SHORT).show();
         }
     }
+
+    class PhotoTask extends AsyncTask<String,Void ,AttributedPhoto> {
+
+        private int mHeight;
+
+        private int mWidth;
+
+        public PhotoTask(int width, int height) {
+            mHeight = height;
+            mWidth = width;
+        }
+
+        /**
+         * Loads the first photo for a place id from the Geo Data API.
+         * The place id must be the first (and only) parameter.
+         */
+        @Override
+        protected void onPreExecute() {
+            // Display a temporary image to show while bitmap is loading.
+            mImageView.setImageResource(R.drawable.bucketdefault);
+        }
+
+        @Override
+        protected AttributedPhoto doInBackground(String... params) {
+            if (params.length != 1) {
+                return null;
+            }
+            final String placeId = params[0];
+            AttributedPhoto attributedPhoto = null;
+
+            PlacePhotoMetadataResult result = Places.GeoDataApi
+                    .getPlacePhotos(mGoogleApiClient, placeId).await();
+
+            if (result.getStatus().isSuccess()) {
+                PlacePhotoMetadataBuffer photoMetadataBuffer = result.getPhotoMetadata();
+                if (result.getPhotoMetadata().getCount() > 0 && !isCancelled()) {
+                    // Get the first bitmap and its attributions.
+                    PlacePhotoMetadata photo = result.getPhotoMetadata().get(0);
+                    CharSequence attribution = photo.getAttributions();
+                    // Load a scaled bitmap for this photo.
+                    Bitmap image = photo.getScaledPhoto(mGoogleApiClient, mWidth, mHeight).await()
+                            .getBitmap();
+
+                    attributedPhoto = new AttributedPhoto(attribution, image);
+                }
+                // Release the PlacePhotoMetadataBuffer.
+                photoMetadataBuffer.release();
+            }
+            return attributedPhoto;
+        }
+
+                // Create a new AsyncTask that displays the bitmap and attribution once loaded.
+
+                    @Override
+                    protected void onPostExecute(AttributedPhoto attributedPhoto) {
+                        if (attributedPhoto != null) {
+                            Toast.makeText(getApplicationContext(), "이미지 로드 됨", Toast.LENGTH_SHORT).show();
+
+                            // Photo has been loaded, display it.
+                            mImageView.setImageBitmap(attributedPhoto.bitmap);
+                            Toast.makeText(getApplicationContext(), attributedPhoto.attribution, Toast.LENGTH_SHORT).show();
+                            // Display the attribution as HTML content if set.
+                            if (attributedPhoto.attribution == null) {
+                                mText1.setVisibility(View.GONE);
+                            } else {
+                                mText1.setVisibility(View.VISIBLE);
+                                mText1.setText(Html.fromHtml(attributedPhoto.attribution.toString()));
+                            }
+
+                        }
+                    }
+                }
+        /**
+         * Holder for an image and its attribution.
+         */
+        class AttributedPhoto {
+
+            public final CharSequence attribution;
+
+            public final Bitmap bitmap;
+
+            public AttributedPhoto(CharSequence attribution, Bitmap bitmap) {
+                this.attribution = attribution;
+                this.bitmap = bitmap;
+            }
+        }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
